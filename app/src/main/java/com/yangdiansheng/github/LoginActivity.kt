d53319c874cfa37595ac79be0cbc1a2956d23992
杨殿生 <893187229@qq.com>
2019-12-07 18:06:37 +0800

    mvp base activity

--- app/src/main/java/com/yangdiansheng/github/LoginActivity.kt
+++ app/src/main/java/com/yangdiansheng/github/LoginActivity.kt
@@ -2,8 +2,6 @@ package com.yangdiansheng.github
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import com.yangdiansheng.mvp.impl.MainFragment
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -12,13 +10,6 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
-
-        val mainFragment = MainFragment()
-        Log.d("mvp", mainFragment.toString())
-        Log.d("mvp", mainFragment.presenter.toString())
-        Log.d("mvp", mainFragment.presenter.view.toString())
-
-        mainFragment.onResume()
 
 
         email.setText(Setting.email)
--- mvp/src/main/java/com/yangdiansheng/mvp/impl/BaseActivity.kt
+++ mvp/src/main/java/com/yangdiansheng/mvp/impl/BaseActivity.kt
@@ -1,18 +1,17 @@
 package com.yangdiansheng.mvp.impl
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import com.yangdiansheng.mvp.IMvpView
 import com.yangdiansheng.mvp.IPresenter
-import java.lang.reflect.ParameterizedType
-import java.lang.reflect.Type
 import kotlin.reflect.KClass
 import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.primaryConstructor
 import kotlin.reflect.jvm.jvmErasure
 
-abstract class BaseFragment<out P : BasePresenter<BaseFragment<P>>> : IMvpView<P>, Fragment() {
-    override val presenter: P
+abstract class BaseActivity<out P: BasePresenter<BaseActivity<P>>> :IMvpView<P>, AppCompatActivity() {
+
+    final override val presenter: P
 
     init {
         presenter = createPresenterKt()
@@ -21,7 +20,7 @@ abstract class BaseFragment<out P : BasePresenter<BaseFragment<P>>> : IMvpView<P
 
     fun createPresenterKt(): P {
         sequence {
-            var thisClass: KClass<*> = this@BaseFragment::class
+            var thisClass: KClass<*> = this@BaseActivity::class
             while (true) {
                 yield(thisClass.supertypes)
                 thisClass = thisClass.supertypes.firstOrNull()?.jvmErasure ?: break
@@ -35,28 +34,12 @@ abstract class BaseFragment<out P : BasePresenter<BaseFragment<P>>> : IMvpView<P
         }
     }
 
-    fun createPresenter(): P {
-        sequence<Type> {
-            var thisClass: Class<*> = this@BaseFragment.javaClass
-            while (true) {
-                yield(thisClass.genericSuperclass)
-                thisClass = thisClass.superclass ?: break
-            }
-        }.filter {
-            it is ParameterizedType
-        }.flatMap {
-            (it as ParameterizedType).actualTypeArguments.asSequence()
-        }.first {
-            it is Class<*> && IPresenter::class.java.isAssignableFrom(it)
-        }.let {
-            return (it as Class<P>).newInstance()
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         presenter.onCreate(savedInstanceState)
     }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {}
 
     override fun onStart() {
         super.onStart()
@@ -88,8 +71,10 @@ abstract class BaseFragment<out P : BasePresenter<BaseFragment<P>>> : IMvpView<P
         presenter.onSaveInstanceState(outState)
     }
 
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        onViewStateRestored(savedInstanceState)
         presenter.onViewStateRestored(savedInstanceState)
     }
+
 }
